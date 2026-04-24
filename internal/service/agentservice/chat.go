5a7ce686d2508496804ee4000392ff1fce755abc
mife <15723556393@163.com>
2026-04-24 23:59:29 +0800

    task

--- internal/service/agentservice/chat.go
+++ internal/service/agentservice/chat.go
@@ -2,10 +2,17 @@ package agentservice
 
 import (
 	"bloger/internal/domain"
+	"bloger/pkg/task"
 	"context"
 )
 
 // Chat 聊天
 func (a *AgentService) Chat(ctx context.Context, input domain.ChatRequest) (domain.ChatResponse, error) {
-	return a.agent.Chat(ctx, input)
+	var ChatResponse domain.ChatResponse
+	err := task.Do(ctx,func() error{
+		var err error
+		ChatResponse,err =  a.agent.Chat(ctx, input)
+		return err
+	}
+	)
 }
